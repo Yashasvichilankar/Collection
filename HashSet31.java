a419c607c08d815930a3a7a66d81f4b9df49d124
Yashasvi <yashasvichilankar27@gmail.com>
2023-05-14 20:41:26 +0530

    adding file

---
+++ HashSet31.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class HashSet31 {
+   public static void main(String args[]) {
+      // create a hash set
+	   HashSet<String> hs = new HashSet<>();
+      // add elements to the hash set
+      hs.add("A");
+	  hs.add("B");
+      hs.add("D");
+	  hs.add("D");
+	  hs.add("C");
+      hs.add("E");
+      hs.add("C");
+      hs.add("F");
+      hs.add("o");
+      hs.add(null);
+      hs.add("0");
+      hs.add("1");
+      hs.add("a");
+      System.out.println(hs);
+      hs.remove("C");
+      System.out.println(hs);
+      hs.clear();
+      System.out.println(hs.isEmpty()); 
+      
+      HashSet<String> al=new HashSet<String>();  
+      al.add("Ravi"); 
+      
+      al.add("Vijay kumar"); 
+      al.add(null);
+      al.add("Vijay");  
+      al.add("Ravi");  
+      al.add("Ajay");   
+      System.out.println(al);
+      Iterator<String> itr=al.iterator();  
+      while(itr.hasNext()){  
+       System.out.println(itr.next());  
+      
+      }
+      
+   }
+}
